e91e249e020580def48ad47c8f7538f1e4723d47
Aryo Skuyyyy <114992377+Aryo1235@users.noreply.github.com>
2024-12-04 00:05:20 +0700

    upadate near by

--- Travel/src/components/common/DestinationCard.jsx
+++ Travel/src/components/common/DestinationCard.jsx
@@ -9,7 +9,7 @@ const DestinationCard = ({ destination }) => {
         <img
           src={destination.images[0]?.url}
           alt={destination.name}
-          className="w-[330px] h-[180px] rounded-md hover:opacity-90 transition"
+          className="w-[330px] h-[180px] rounded-md hover:opacity-90 transition object-cover object-center"
         />
       </Link>
       <div className="mt-2">
@@ -17,7 +17,7 @@ const DestinationCard = ({ destination }) => {
         <h5 className="text-md font-bold leading-tight line-clamp-1 text-sky-900 dark:text-white mb-1">
           <Link to={`/home/${destination.id}`}>{destination.name}</Link>
         </h5>
-        <p className="text-sm leading-tight text-gray-700 dark:text-gray-400">
+        <p className="text-sm leading-tight text-gray-700 dark:text-gray-400 line-clamp-1">
           {destination.address}
         </p>
       </div>
