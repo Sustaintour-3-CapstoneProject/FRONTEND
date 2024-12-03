2af6b289db162d30c1a94a3af2d653e8d55568d8
Aryo Skuyyyy <114992377+Aryo1235@users.noreply.github.com>
2024-12-03 19:13:35 +0700

    Update NearBy.jsx

--- Travel/src/components/User/HomePage/NearBy.jsx
+++ Travel/src/components/User/HomePage/NearBy.jsx
@@ -5,7 +5,7 @@ import destinations from "../../../data/destinationData";
 
 const NearByDestinations = () => {
   const [startIndex, setStartIndex] = useState(0);
-  const [itemsToShow, setItemsToShow] = useState(4); // Default: 4 item (desktop)
+  const [itemsToShow, setItemsToShow] = useState(6); // Default: 4 item (desktop)
 
   useEffect(() => {
     // Fungsi untuk menentukan jumlah item berdasarkan ukuran layar
@@ -13,7 +13,7 @@ const NearByDestinations = () => {
       if (window.innerWidth < 640) {
         setItemsToShow(1); // Mobile: tampilkan 1 item
       } else {
-        setItemsToShow(4); // Desktop: tampilkan 4 item
+        setItemsToShow(6); // Desktop: tampilkan 4 item
       }
     };
 
@@ -47,56 +47,59 @@ const NearByDestinations = () => {
   return (
     <section className="py-10">
       <h2 className="text-2xl font-bold mb-6">Near By Destinations</h2>
-      <div className="relative flex items-center">
-        {/* Tombol Previous */}
-        <button
-          onClick={prevItem}
-          className="mr-2 left-0 bg-gray-800 text-white p-2 rounded-full z-10
-            sm:p-2 sm:w-10 sm:h-10
-             w-8 h-8"
-        >
-          <FaChevronLeft size={16} className="sm:size-[24px]" />
-        </button>
+      <div className="relative ">
+        {/* Wrapper Carousel */}
+        <div className="relative flex items-center">
+          {/* Tombol Previous */}
+          <button
+            onClick={prevItem}
+            className="mr-2 left-0 bg-gray-800 text-white p-2 rounded-full z-10
+              sm:p-2 sm:w-10 sm:h-10
+               w-8 h-8"
+          >
+            <FaChevronLeft size={16} className="sm:size-[24px]" />
+          </button>
 
-        {/* Konten Carousel */}
-        <div className="flex gap-4 overflow-hidden justify-center w-full">
-          {carouselItems.map((destination) => (
-            <div
-              key={destination.id}
-              className="min-w-full sm:min-w-[265px] sm:max-w-[265px]"
-            >
-              <div className="border border-x-gray-100 rounded-lg shadow-lg p-4 bg-white">
-                <Link to={`/home/${destination.id}`}>
-                  <img
-                    src={destination.images[0]?.url}
-                    alt={destination.name}
-                    className="w-full h-[180px] sm:w-[265px] sm:h-[180px] rounded-md hover:opacity-90 transition"
-                  />
-                </Link>
-                <div className="mt-2">
-                  <h5 className="text-md font-bold leading-tight line-clamp-1 text-sky-900 dark:text-white mb-1">
-                    <Link to={`/home/${destination.id}`}>
-                      {destination.name}
-                    </Link>
-                  </h5>
-                  <p className="text-sm leading-tight text-gray-700 dark:text-gray-400">
-                    {destination.address}
-                  </p>
+          {/* Konten Carousel */}
+          <div className="flex justify-center gap-4 overflow-x-hidden w-full ">
+            {carouselItems.map((destination) => (
+              <div
+                key={destination.id}
+                className="flex-shrink-0 w-full sm:min-w-[200px] sm:max-w-[200px]"
+              >
+                <div className="border border-x-gray-100 rounded-lg shadow-lg p-4 bg-white">
+                  <Link to={`/home/${destination.id}`}>
+                    <img
+                      src={destination.images[0]?.url}
+                      alt={destination.name}
+                      className="w-full h-[180px] sm:h-[150px] rounded-md hover:opacity-90 transition"
+                    />
+                  </Link>
+                  <div className="mt-2">
+                    <h5 className="text-md font-bold leading-tight line-clamp-1 text-sky-900 dark:text-white mb-1">
+                      <Link to={`/home/${destination.id}`}>
+                        {destination.name}
+                      </Link>
+                    </h5>
+                    <p className="text-xs leading-tight text-gray-700 dark:text-gray-400">
+                      {destination.address}
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
 
-        {/* Tombol Next */}
-        <button
-          onClick={nextItem}
-          className="ml-2 right-0 bg-gray-800 text-white p-2 rounded-full z-10
-            sm:p-2 sm:w-10 sm:h-10
-             w-8 h-8"
-        >
-          <FaChevronRight size={16} className="sm:size-[24px]" />
-        </button>
+          {/* Tombol Next */}
+          <button
+            onClick={nextItem}
+            className="ml-2 left-0 bg-gray-800 text-white p-2 rounded-full z-10
+              sm:p-2 sm:w-10 sm:h-10
+               w-8 h-8"
+          >
+            <FaChevronRight size={16} className="sm:size-[24px]" />
+          </button>
+        </div>
       </div>
     </section>
   );
