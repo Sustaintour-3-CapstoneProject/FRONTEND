b03c6f7653abfb7d3add1169c3bea1c65bcb5b94
Aryo Skuyyyy <114992377+Aryo1235@users.noreply.github.com>
2024-12-08 04:23:10 +0700

    update navbar

--- Travel/src/components/User/HomePage/Navbar.jsx
+++ Travel/src/components/User/HomePage/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { Navbar, Button, Dropdown, Avatar } from "flowbite-react";
 import { NavLink, useLocation } from "react-router-dom";
 import useAuthStore from "../../../store/authStore";
@@ -5,6 +6,17 @@ import useAuthStore from "../../../store/authStore";
 const NavigationBar = () => {
   const { auth, clearAuth } = useAuthStore(); // Ambil data autentikasi dan fungsi logout
   const location = useLocation(); // Ambil lokasi saat ini
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  // Pantau scroll
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0); // Jika scroll lebih dari 0, ubah state
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll); // Bersihkan listener
+  }, []);
 
   const handleLogout = () => {
     clearAuth(); // Hapus data autentikasi
@@ -12,8 +24,12 @@ const NavigationBar = () => {
   };
 
   return (
-    <div className="sticky top-0 z-30">
-      <Navbar fluid rounded className="shadow-lg">
+    <div
+      className={`sticky top-0 z-30 transition-all duration-300 ${
+        isScrolled ? "shadow-lg border-b border-gray-200" : ""
+      }`}
+    >
+      <Navbar fluid rounded className="bg-white">
         <Navbar.Brand href="/">
           <img
             src="/logo2.png" // Sesuaikan dengan logo Anda
